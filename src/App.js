a21965eaed1da887b78e2c106a25bc0f0e41fae5
Richard Soto <richsoto123@gmail.com>
2020-08-04 19:42:31 -0400

    completed, but pause button not working

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      pauseTicker: false,
+    }
+  }
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({
+        count: this.state.count +1,
+      })
+    }, 1000)
+  }
+  
+  shouldComponentUpdate (nextProps, nextState){
+    if (nextState.count % 3 === 0 ) return true;
+    else return false;
+
+  }
+
+  reset = () => { this.setState({
+    //reset count to zero
+    count:  0
+  });
+   
+  }
+  pause = () => {
+    if(this.state.pauseTicker){
+      this.setState({
+        pauseTicker: false,
+      })
+    }else{
+      this.setState({
+        pauseTicker: true,
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>The ticker number is : {this.state.count}</p>
+        <button type = "button" onClick={this.reset}> Reset Ticker</button>
+        <button type = "button" onClick={this.pause}> 
+        {this.state.pauseTicker ? 'Ticker is paused' : 'Pause the Ticker'}</button>
+      </div>
+    );
+  }
+}
+
+export default Ticker 
